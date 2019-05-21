d1aed58b186476e4baf4f1e5bbd3f0a15e63050d
Jacob Ogren <jogren0044@gmail.com>
2019-05-21 16:06:38 -0600

    Move variables from idea.js to main.js

--- idea.js
+++
@@ -1,7 +0,0 @@
-var starredIdeasBtn = document.getElementById('starred-ideas-button');
-var newQualityInput = document.getElementById('quality-input');
-var addQualityBtn = document.getElementById('add-quality-button');
-var titleInput = document.getElementById('title-input');
-var bodyInput = document.getElementById('body-input');
-var saveBtn = document.getElementById('save-button');
-var searchInput = document.getElementById('search-ideas-input');
